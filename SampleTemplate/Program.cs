0bc1ff26a04a22b45fbd4e2578defb389ccc62b7
Мария Мешкова <165886191+Mrueya@users.noreply.github.com>
2025-04-26 13:45:11 +0300

    Add files via upload

---
+++ SampleTemplate/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SampleTemplate.Food;
+Console.WriteLine("Горячий дог");
+new HotDog().PrepareFood();
+Console.WriteLine("Гамбургер");
+new Hamburger().PrepareFood();
