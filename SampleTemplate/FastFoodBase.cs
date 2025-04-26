a65d7095288106a77045b6e0f9d7ba4ac46eccfa
Мария Мешкова <165886191+Mrueya@users.noreply.github.com>
2025-04-26 13:41:56 +0300

    Add files via upload

---
+++ SampleTemplate/FastFoodBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTemplate.Food
+{
+    public abstract class FastFoodBase
+    {
+        public void PrepareFood()
+        {
+            Console.WriteLine("Готовим еду:");
+            AddBread();
+            AddSalat();
+            AddMeat();
+            if(WantsTopping())
+            {
+                AddTopping();
+            }
+        }
+        private void AddBread()
+        {
+            Console.WriteLine("Добавлен хлеб");
+        }
+        public abstract void AddSalat();
+        public abstract void AddMeat();
+        public abstract void AddTopping();
+        public virtual bool WantsTopping()
+        {
+            return true;
+        }
+
+    }
+}
---
+++ SampleTemplate/Hamburger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTemplate.Food
+{
+    public class Hamburger : FastFoodBase
+    {
+        public override void AddMeat()
+        {
+            Console.WriteLine("Добавлена котлета");
+        }
+
+        public override void AddSalat()
+        {
+            Console.WriteLine("Добавлены листья салата");
+        }
+
+        public override void AddTopping()
+        {
+            Console.WriteLine("Добавлен соус тысяча островов");
+        }
+    }
+}
