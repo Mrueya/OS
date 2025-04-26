84286472c166c4a8ee2df829c425fc8a24dfc33d
Мария Мешкова <165886191+Mrueya@users.noreply.github.com>
2025-04-26 13:38:08 +0300

    Add files via upload

---
+++ HotDog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTemplate.Food
+{
+    public class HotDog : FastFoodBase
+    {
+        public override void AddMeat()
+        {
+            Console.WriteLine("Добавлена сосиска");
+        }
+
+        public override void AddSalat()
+        {
+            Console.WriteLine("Добавлен жаренный лук");
+        }
+
+        public override void AddTopping()
+        {
+            Console.WriteLine("Добавлена горчица");
+        }
+
+        public override bool WantsTopping()
+        {
+            Console.WriteLine("Нужна горчица?");
+            var readLine = Console.ReadLine();
+            if (readLine != null || readLine.ToLower().Equals("да"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
